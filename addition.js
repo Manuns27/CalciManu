eaa075565210baa4a5e33d3d04d02e3f09944e7f
Saleem3097 <saleem3097@gmail.com>
2022-04-14 13:36:42 +0530

    did implemented add and sub function

---
+++ addition.js
@@ -0,0 +1,7 @@
+function add(){
+var a  = docunent.getElementById("inp1").value;
+var b = document.getElenentById("inp2").value;
+var res = parseInt(a) + parseInt(b);
+document.getElementById("res").value = res;
+}
+
---
+++ subtraction.js
@@ -0,0 +1,6 @@
+function add(){
+var a  = docunent.getElementById("inp1").value;
+var b = document.getElenentById("inp2").value;
+var res = parseInt(a) - parseInt(b);
+document.getElementById("res").value = res;
+}
